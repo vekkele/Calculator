dfbaa7552963a24ada51ad9c27f130ce7f76671c
Vlad Keliy <limonkeliy153@gmail.com>
2017-09-30 18:33:49 +0300

    Add calculating functionaity

--- project/js/script.js
+++ project/js/script.js
@@ -1,17 +1,14 @@
 $(document).ready(function () {
   "use strict";
 
-  var btnValue,
-    inputText,
-    inputLastSymbol,
-    numbers;
-
 
   $('button').on('click', function () {
-    btnValue        = $(this).attr('value');
-    inputText       = $('#input').val();
-    inputLastSymbol = inputText[inputText.length - 1];
-    numbers         = inputText.match(/\d+\.?\d*/g);
+    var btnValue = $(this).attr('value'),
+      inputText = $('#input').val(),
+      inputLastSymbol = inputText[inputText.length - 1],
+      numbers = inputText.match(/\d+\.?\d*/g),
+      signs = inputText.match(/[-+\*/]/g);
+
 
     if (!isNaN(parseInt(btnValue))) {
       $('#input').val(inputText + btnValue);
@@ -40,11 +37,46 @@ $(document).ready(function () {
 
     if (btnValue == '.' && !isNaN(parseInt(inputLastSymbol)) &&
       numbers.slice(-1)[0].search(/\./) == -1) {
+
       $('#input').val(inputText + btnValue);
+    }
+
+    if (btnValue == '=') {
+      var result = 0,
+        num1 = getNumFromString(numbers[0]),
+        num2;
+      console.log(numbers);
+      console.log(signs);
+      for (var i = 0; i < (numbers.length - 1); i++) {
+        num2 = getNumFromString(numbers[i + 1]);
+        result = calculateTwoNums(num1, num2, signs[i]);
+        num1 = result;
+      }
+      $('#input').val(result);
     }
 
 
   });
+
+  function getNumFromString(numberStr) {
+    if (numberStr.search(/\./) == -1) {
+      return parseInt(numberStr);
+    }
+    return parseFloat(numberStr);
+  }
+
+  function calculateTwoNums(num1, num2, sign) {
+    switch (sign) {
+      case '+':
+        return num1 + num2;
+      case '-':
+        return num1 - num2;
+      case '*':
+        return num1 * num2;
+      case '/':
+        return num1 / num2;
+    }
+  }
 
 
 });
